016eba19fba9d83a274ff7513ec4e7a9d475c5b9
Alexis Moreno <alexis.morenotrejo@gmail.com>
2023-05-22 14:31:56 -0600

    ci: HasMap y HasSet

--- java08-colecciones/src/org/generation/HashSetConceptos.java
+++ java08-colecciones/src/org/generation/HashSetConceptos.java
@@ -1,5 +1,7 @@
 package org.generation;
 
+import java.util.HashSet;
+
 public class HashSetConceptos {
 
 	public static void main(String[] args) {
@@ -11,6 +13,37 @@ public class HashSetConceptos {
 		 * Sintaxis:
 		 * 	HashSet<Object> varName;
 		 */
+		
+		// Definimos una colección HashSet
+		HashSet<String> paises = new HashSet<>();
+		
+		// Agregar valores con add()
+		paises.add("México");
+		paises.add("Alemania");
+		paises.add("Genovia");
+		paises.add("Wakanda");
+		
+		System.out.println(paises);
+		paises.add("Genovia"); // Agregando duplicado
+		System.out.println(paises); // No se agrego porque ya existia
+		
+		// Desplegamos el tamaño de la colección, size()
+		System.out.println("Elementos: " + paises.size());
+		
+		// Verificando si existe un elemento, contains()
+		System.out.println("Existe USA: " + paises.contains("USA"));
+		System.out.println("Existe Alemania: " + paises.contains("Alemania"));
+		
+		// Removemos un elemento, remove()
+		System.out.println("Eliminamos \"Mexico\": " + paises.remove("Mexico"));
+		System.out.println(paises);
+		
+		// Iteramos la colección
+		// for each loop
+		for (String pais : paises) System.out.println(pais);
+		
+		// Usando lambda expressions
+		paises.forEach( pais -> System.out.println("->" + pais) );
 
 	}
 
