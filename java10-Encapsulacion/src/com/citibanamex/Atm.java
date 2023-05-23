13bc4974ae5c4ed7d035a4ed498770f8e6401fdd
Alexis Moreno <alexis.morenotrejo@gmail.com>
2023-05-23 12:45:38 -0600

    atributos protegidos

---
+++ java10-Encapsulacion/src/com/citibanamex/Atm.java
@@ -0,0 +1,23 @@
+package com.citibanamex;
+
+public class Atm {
+	private int serialNumber;
+	private double balance;
+	
+	private static int counterAtm = 1;
+	
+	public Atm() {
+		this.balance = 100_000;
+		this.serialNumber = counterAtm++;
+	}
+ 
+	public Atm(double balance){
+		this(); // se invoca al constructor sin parámetros (default)
+		this.balance = balance;
+	}
+
+	public Atm(String balance){
+		this(Double.parseDouble(balance));
+	}
+	
+}
---
+++ java10-Encapsulacion/src/org/generation/test/AtmTest.java
@@ -0,0 +1,26 @@
+package org.generation.test;
+
+import com.citibanamex.Atm;
+
+public class AtmTest {
+
+	public static void main(String[] args) {
+		System.out.println("Counter Atm: " + Atm.counterAtm);
+		
+		Atm cajeroA = new Atm(9_000.00);
+		// Al usar el constructor por default, se agregue
+		// 100_000 al atributo balance.
+		Atm cajeroB = new Atm();
+		Atm cajeroC = new Atm("777000");
+		
+		
+		System.out.println(cajeroA.serialNumber);
+		System.out.println(cajeroA.balance);
+		System.out.println(cajeroB.serialNumber);
+		System.out.println(cajeroB.balance);
+		System.out.println(cajeroC.serialNumber);
+		System.out.println(cajeroC.balance);
+		
+	}
+
+}
